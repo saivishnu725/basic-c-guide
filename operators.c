75c227909113225a7b9535511d4002e110881a29
saivishnu725 <saivishnu725@gmail.com>
2026-06-26 19:45:26 +0530

    ternary operator inside printf

--- operators.c
+++ operators.c
@@ -2,10 +2,12 @@
 
 int main() {
 	int x, y;
-	x = 12;
+	x = 11;
 	y = x > 10 ? 5 : 15;
 	printf("if x=12, y: %d\n", y);
+	printf("x=%d is %s\n", x, x % 2 == 0 ? "even" : "odd");
 	x = 8;
 	y = x < 10 ? 5 : 15;
 	printf("if x=8, y: %d\n", y);
+	printf("x=%d is %s\n", x, x % 2 == 0 ? "even" : "odd");
 }
