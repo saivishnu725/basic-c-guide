31beaaf5e4b3a19875ac6855d6fc546631db23b8
saivishnu725 <saivishnu725@gmail.com>
2026-06-26 19:54:29 +0530

    sizeof()

--- operators.c
+++ operators.c
@@ -10,4 +10,9 @@ int main() {
 	y = x < 10 ? 5 : 15;
 	printf("if x=8, y: %d\n", y);
 	printf("x=%d is %s\n", x, x % 2 == 0 ? "even" : "odd");
+
+	// size of
+	printf("size of int: %zu\n", sizeof(x));
+	printf("size of float: %zu\n", sizeof(2.14));
+	printf("size of char: %zu\n", sizeof('c'));
 }
