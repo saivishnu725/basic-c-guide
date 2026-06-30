b3c3f66dc611899b3def3982151c4853ad3de757
saivishnu725 <saivishnu725@gmail.com>
2026-06-30 22:02:46 +0530

    pointer I; 2 more to go

--- pointers.c
+++ pointers.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+void increment(int *n){ // get pointer-to-int
+	*n = *n + 1; // dereference and increment
+	// no need to send it back (return) as it has already inc in the memory
+}
+
 int main(void) {
 	int i = 10; // regular var
 	int *p; // pointer-to-int
@@ -8,4 +13,10 @@ int main(void) {
 	printf("&i: %p\n", &i); // &i is address
 	printf("p: %p\n", p);
 	printf("*p: %d\n", *p); // dereferencing the pointer
+
+	// increment i using pointer
+	increment(p);
+	printf("i: %d\n", i);
+	printf("*p: %d\n", *p);
 }
+
