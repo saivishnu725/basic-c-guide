55ef419ee4152228efb6994261b50472c612ebf2
saivishnu725 <saivishnu725@gmail.com>
2026-07-08 18:32:18 +0530

    char to int; casting

--- conversions.c
+++ conversions.c
@@ -41,7 +41,6 @@ int main(void) {
 		printf("e: %s\n", error_string); // e: q34
 		printf("%s is not a proper number so fix it\n", s3);
 	}		
-
 	// same as above but with a proper number
 	s3 = "1234";
 	// char *error_string;
@@ -51,4 +50,47 @@ int main(void) {
 		printf("e: %s\n", error_string); 
 		printf("%s is not a proper number so fix it\n", s3);
 	}
+
+	// char to int
+	// char is just ASCII int
+	// 'A' == 65; 'a' == 97
+	// 'Z' == 90; 'z' == 122
+	// similarly a digit character also has its own ASCII value
+	// '0' == 48; '9' == 57
+	// imagine you want int 4, we can subtract '4' - '0'
+	// which would be the same as 52 - 48 = 4 (arithmetic on char are done same as int type) 
+	// this trick can be used to convert char to int
+	char c = '4';
+	a = c; // holds 52
+	printf("ASCII val a: %d\n", a); // ASCII val a: 52
+	a = c - '0'; // 52 - 48 == 4
+	printf("converted a: %d\n", a); // converted a: 4
+
+	// casting
+	// if you want to explicitly mention the type you want to convert to
+	// mention the type in `()` before the expression
+	printf("this is int: %d\n", (2 + 3));
+	printf("but you can make it float %f by mentioning (float) at the start\n", (float)(2 + 3));
+	// you can use this with void* to cast it to any type
+	int p = 123; // normal int
+	void *pv = &p; // int-address in void pointer (void can hold ANY type)
+	int *pi = (int *)pv; // convert void-pointer to int-pointer
+	//int *pi = pv;	     // it can auto convert without casting
+	printf("*pi: %d\n", *pi);
+	// if you use `%p` to print address saved in pointer
+	// you have to cast it back to (void *) or you will get a warning
+	printf("pv: %p\n", pv);
+	printf("pi (yes cast; no warning): %p\n", (void *)pi);
+	// printf("pi (no cast; yes warning): %p\n", pi);
+	/*
+	warning: format ‘%p’ expects argument of type ‘void *’, but argument 2 has type ‘int *’ [-Wformat=]
+	      |         printf("pi (no cast; warning): %p\n", pi);
+	      |                                        ~^     ~~
+	      |                                         |     |
+	      |                                         |     int *
+	      |                                         void *
+	      |                                        %ls
+	*/
+
 }
+
